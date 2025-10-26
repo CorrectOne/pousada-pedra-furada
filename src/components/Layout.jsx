fdb6e15a881c4b5665a425e3a126504f48a8c5e6
CorrectOne <66885539+CorrectOne@users.noreply.github.com>
2025-10-26 00:39:22 +0000

    “refactor: Refatoração do Layout.jsx, Header.jsx e criação do componente Footer.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import Header from "./Header";
-
-export default function Layout({ children }) {
-  return (
-    <>
-      <Header/>
-      <main>{children}</main>
-    </>
-  );
-}
+import React from "react"; 
+import Header from "./Header"; 
+import Footer from "./Footer"; 
+export default function Layout({ children }) { 
+ return ( 
+ <div className="layout"> 
+ <Header /> 
+ <main style={{ padding: "1rem" }}> 
+ {children} 
+ </main> 
+ <Footer /> 
+ </div> 
+ ); 
+} 
